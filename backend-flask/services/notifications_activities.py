a3de4a6eee2d36beec0a6625b1417c48cf6109cc
Ejike Mary-cynthia <genejike2014@gmail.com>
2023-03-01 18:52:13 +0000

    notifications update

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta, timezone
 class NotificationsActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()    results = [{
+    now = datetime.now(timezone.utc).astimezone() 
+    results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'coco',
       'message': 'i am a red corn!',
