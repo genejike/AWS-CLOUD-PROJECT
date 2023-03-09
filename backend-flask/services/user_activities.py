eb40996f55cdc213306b70d122e9ee859c3609d2
genejike <genejike2014@gmail.com>
2023-03-09 01:51:27 +0100

    commented xray deamon

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -4,7 +4,7 @@ from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
       # xray----
-   segment = xray_recorder.begin_segment('user_activities')
+   # segment = xray_recorder.begin_segment('user_activities')
   model = {
       'errors': None,
       'data': None
@@ -26,11 +26,11 @@ class UserActivities:
       model['data'] = results
 
          # xray---
-   dict = {
-      "now": now .isoformat(),
-      "results-size":len(model['data'])
-    }
+   #dict = {
+     # "now": now .isoformat(),
+      #"results-size":len(model['data'])
+    #}
 
-    subsegment = xray_recorder.begin_subsegment('mock-data')
+   # subsegment = xray_recorder.begin_subsegment('mock-data')
     
     return model
