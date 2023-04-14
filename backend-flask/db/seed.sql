c0fd6dabea040acd4b75fcc462f917c768a75605
Ejike Mary-cynthia <genejike2014@gmail.com>
2023-04-14 09:08:31 +0000

    utility scripts working with dynamo db

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK'),
-  ('genejike', 'cynthy' ,'MOCK');
+  ('Andrew Brown', 'andrew@exampro.co','andrewbrown','MOCK'),
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko','MOCK'),
+  ('genejike', 'genejike2014@gmail.com', 'cynthy','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
